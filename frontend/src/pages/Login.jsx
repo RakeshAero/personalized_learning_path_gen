bdfdd8d293a9dabc5e6c5882e860517a2f2eed3a
RakeshAero <rakeshaero55743@gmail.com>
2026-05-21 17:41:30 +0530

    Login Page Started

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -11,6 +11,13 @@ function Login() {
             if(!username || !password){
                 setError('Please enter both username and password');
             }
+
+            const response = await API.post('users/login/',{
+                username,
+                password
+            });
+
+            console.log(response.data);
         }
         catch(error){
             console.log("Error ",error.message);
@@ -21,9 +28,52 @@ function Login() {
 
     return (
         <div className="flex items-center justify-center min-h-screen">
-            <h1 className="text-2xl font-bold mb-4">Login Page</h1>
+            <div className="border p-8 rounded">
+                <h1 className="text-2xl font-bold mb-4">Login Page</h1>
+            </div>
+
+            <input 
+                type="text" 
+                className="border p-2 w-full mb-3" 
+                placeholder="Username"
+                onChange={(e) => setUsername(e.target.value)}
+                />
         </div>
     );
 }
 
 export default Login;
+
+
+{/* <div className="flex items-center justify-center min-h-screen">
+
+            <div className="border p-8 rounded">
+
+                <h1 className="text-2xl font-bold mb-4">
+                    Login
+                </h1>
+
+                <input
+                    type="text"
+                    placeholder="Username"
+                    className="border p-2 w-full mb-3"
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+
+                <input
+                    type="password"
+                    placeholder="Password"
+                    className="border p-2 w-full mb-3"
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+
+                <button
+                    onClick={handleLogin}
+                    className="bg-black text-white px-4 py-2 rounded w-full"
+                >
+                    Login
+                </button>
+
+            </div>
+
+        </div> */}
